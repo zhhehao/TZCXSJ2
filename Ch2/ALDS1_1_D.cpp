704b3adc9c6dce6d600da3f2318038a07cfd5b64
zhhehao <zh.hehao@gmail.com>
2018-01-05 18:04:45 +0800

    submit error

--- Ch2/ALDS1_1_D.cpp
+++ Ch2/ALDS1_1_D.cpp
@@ -1,9 +1,34 @@
 # include <cstdio>
 # include <cstring>
 
-int main(void) {
-    freopen("data.in", "r", stdin);
-    freopen("data.out", "w", stdout);
+int ri, rj, ans, n;
 
+int main(void) {
+    // freopen("data.in", "r", stdin);
+    // freopen("data.out", "w", stdout);
+    while (scanf("%d", &n) == 1) {
+        scanf("%d%d", &ri, &rj);
+        ans = rj - ri;
+        int t, na;
+        int len = n - 2;
+        for (int i = 0; i < len; i++) {
+            scanf("%d", &t);
+            if (t > rj) {
+                rj = t;
+                na = rj - ri;
+                if (na > ans) ans = na;
+            }
+            else if (t < ri) {
+                ri = t;
+                if (i != len - 1) {
+                    scanf("%d", &rj);
+                    na = rj - ri;
+                    if (na > ans) ans = na;
+                    i++;
+                }
+            }
+        }
+        printf("%d\n", ans);
+    }
     return 0;
 }
