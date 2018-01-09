3feba9c5c1c3053fcdfa46b63f5097515b3d0c88
zhhehao <zh.hehao@gmail.com>
2018-01-09 14:52:58 +0800

    ac

---
+++ Ch2/ALDS1_3_B.cpp
@@ -0,0 +1,51 @@
+/*
+* 挑战程序设计竞赛2 第四章 第二题 queue
+* http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_3_B
+*/
+
+# include <cstdio>
+
+static const int maxn = 10000000;
+int Q[maxn], n, q, head, tail;
+
+struct Job {
+	char name[15];
+	int time;
+} job[100005];
+
+void dequeue() {
+	head++;
+}
+
+void enqueue(int x) {
+	Q[++tail] = x;
+}
+
+int main() {
+	// 输入输出重定向
+	// freopen("data.in", "r", stdin);
+	// freopen("data.out", "w", stdout);
+	
+	scanf("%d%d", &n, &q); 
+	for (int i = 0; i < n; i++) {
+		scanf("%s%d", job[i].name, &job[i].time);
+		Q[i] = i;
+	}
+	head = 0; tail = n-1; // 初始化队列的头和尾
+	int p, t = 0; // p是正在操作的任务，t是目前的时间
+	while (head <= tail) {
+		p = Q[head];
+		if (job[p].time <= q) {
+			t += job[p].time;
+			printf("%s %d\n", job[p].name, t);
+			dequeue();
+		}
+		else {
+			t += q;
+			job[p].time -= q;
+			enqueue(p);
+			dequeue();
+		}
+	}
+	return 0;
+}
